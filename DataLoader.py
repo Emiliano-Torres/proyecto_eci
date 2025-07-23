844aee7bcb46886e28088edd81b85601843f87d8
Emiliano-Torres <emilianomtorres1@gmail.com>
2025-07-23 16:33:35 -0300

    agrego data_loader

---
+++ DataLoader.py
@@ -0,0 +1,13 @@
+"""ESTE ARCHIVO MANEJARA LA CARGA DE INFORMACION Y HARA FUNCIONES BASICAS
+   DE Procesado 
+"""
+import pandas as pd
+def leer_csv(nombre):
+    data=pd.read_csv(nombre+".csv")
+    atributos=data.columns
+    columnas=[]
+    for atributo in atributos:
+        columnas.append(atributo.lower())
+    data.columns=columnas
+    return data
+
